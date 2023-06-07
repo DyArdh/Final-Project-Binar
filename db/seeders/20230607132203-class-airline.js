3a6f26ea4fcb715cd13a69d6eaff4030e48183c2
Andiko Yoga <90024747+DyArdh@users.noreply.github.com>
2023-06-07 21:46:17 +0700

    hotfix: seeder table name

--- db/seeders/20230607132203-class-airline.js
+++ db/seeders/20230607132203-class-airline.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
-      'ClassAirlines',
+      'Class_Airlines',
       [
         {
           airline_id: 1,
@@ -92,6 +92,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('ClassAirlines', null, {});
+    await queryInterface.bulkDelete('Class_Airlines', null, {});
   },
 };
