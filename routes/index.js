c45cd1c380a439c43aebaa9e19e029b07fe44764
Alien-404 <ricoe72@gmail.com>
2023-06-14 19:39:52 +0700

    info: fix conflict

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,8 @@ const authRouter = require('./auth.route');
 const airportRouter = require('./airport.route');
 const airlinesRouter = require('./airlines.route');
 const flightsRouter = require('./flight.route');
-<<<<<<< HEAD
 const notifsRouter = require('./notif.route');
-=======
 const usersRouter = require('./user.route');
->>>>>>> 6e5cf542440a049077618a83abe4f79fd7338fc2
 
 // middleware
 const authMiddleware = require('../middleware/auth.middleware');
@@ -28,12 +25,10 @@ router.use('/airlines', airlinesRouter);
 // flight route
 router.use('/flights', flightsRouter);
 
-<<<<<<< HEAD
 // notif
 router.use('/notifications', authMiddleware, notifsRouter);
-=======
+
 // user route
 router.use('/user', authMiddleware, usersRouter);
->>>>>>> 6e5cf542440a049077618a83abe4f79fd7338fc2
 
 module.exports = router;
